f070aaeee0aa6fadbb2ffa5afe082a04b43fd50f
David Mwangi Mathenge <david.mathenge98@gmail.com>
2017-04-23 20:01:54 +0300

    main app index page refactoring and template

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
 
-    return HttpResponse('<h1>Welcome Explorer!!!</h1>')
+    return render(request, 'main_app/index.html')
