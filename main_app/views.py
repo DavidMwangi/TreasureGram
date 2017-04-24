c41e9add196081346a81eebd6663bc48c636db71
David Mwangi Mathenge <david.mathenge98@gmail.com>
2017-04-24 07:24:04 +0300

    Dynamic Data and Static files

--- main_app/views.py
+++ main_app/views.py
@@ -4,4 +4,21 @@ from django.shortcuts import render
 
 def index(request):
 
-    return render(request, 'main_app/index.html')
+    return render(request, 'main_app/index.html', {'treasures':treasures})
+
+#DYNAMIC DATA
+
+class Treasure():
+
+    def __init__(self, name, value, material,location):
+        self.name = name
+        self.value = value
+        self.material = material
+        self.location = location
+
+treasures = [
+
+    Treasure('Gold Nugget', 500.00, 'gold', 'Curly Creek NM'),
+    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Fall, CO"),
+    Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA'),
+]
