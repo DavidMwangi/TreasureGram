3cb35a4ef98b313e65d4ce3c6b1f600e6f0496af
DavidMwangi <david.mathenge98@gmail.com>
2017-04-24 13:40:20 +0300

    Styling

--- main_app/views.py
+++ main_app/views.py
@@ -10,15 +10,16 @@ def index(request):
 
 class Treasure():
 
-    def __init__(self, name, value, material,location):
+    def __init__(self, name, value, material,location, img_url):
         self.name = name
         self.value = value
         self.material = material
         self.location = location
+        self.img_url = img_url
 
 treasures = [
 
-    Treasure('Gold Nugget', 500.00, 'gold', 'Curly Creek NM'),
-    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Fall, CO"),
-    Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA'),
+    Treasure('Gold Nugget', 500.00, 'gold', 'Curly Creek NM', ""),
+    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Fall, CO", ""),
+    Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA', ""),
 ]
