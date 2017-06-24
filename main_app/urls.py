b091ef34d2e60e9dc82eed25719b9c65d7a8eb33
David Mwangi Mathenge <david.mathenge98@gmail.com>
2017-06-24 13:23:39 +0300

    Detail View Page complete. Links from homepage in place. Links via treasure id

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,20 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name = 'index'),
+    url(r'^([0-9]+)/$', views.detail, name = 'detail'),
 ]
+
+""""
+        Second url looks for a number and captures that value and
+        sends it to the detail view.
+
+        The current index url does not look for any path meaning 
+        will just match localhost
+
+        [0-9]+ will match any number
+        () captures the value which will then be passed as a parameter
+        to the detail view. --> localhost/10
+
+
+"""
--- main_app/views.py
+++ main_app/views.py
@@ -5,8 +5,33 @@ from .models import Treasure
 
 def index(request):
 
+    """"
+
+        Query set fetches all treasure objects from our models
+        Treasures are passed to index.html template to be rendered
+
+    """
+
     treasures = Treasure.objects.all()
     return render(request, 'main_app/index.html', {'treasures':treasures})
+
+def detail(request, treasure_id):
+
+    """
+
+    :param request:
+
+    :param treasure_id: Receives a number value from url dispatcher in the form localhost/value
+     Assigns value to parameter treasure_id. Using the query set below, the treasure with the
+     treasure_id is retrieved from our model and assigned variable treasure.
+
+    :return: Rendered detail.html template for the treasure retrieved
+
+    """
+
+    treasure = Treasure.objects.get(id=treasure_id)
+
+    return render(request, 'main_app/detail.html', {'treasure':treasure})
 
 
 """
